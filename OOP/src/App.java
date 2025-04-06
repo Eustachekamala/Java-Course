9c16020074b25e1238c7b14ffe1975dc94892c83
eustachekamala <eustachejared25@gmail.com>
2025-04-06 16:05:31 +0300

    Created an object car and

---
+++ OOP/src/App.java
@@ -0,0 +1,14 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        /*
+         * Object = an entity that holds data (attributes)
+         * and can perform actions (methods).
+         * It is a reference data type;
+         */
+
+        Car car = new Car();
+
+        car.drive();
+    }
+
+}
