9e9d9596a4392c00a573d10c33bb87f57af42b24
eustachekamala <eustachejared25@gmail.com>
2025-04-07 15:45:09 +0300

    Worked with inheritance that allow one class to inherits attributes and methods for another class(parent)

---
+++ Inheritance/src/Dog.java
@@ -0,0 +1,7 @@
+public class Dog extends Animal{
+    int lives = 9;
+
+    void speak(){
+        System.out.println("The Dog goes *woof*");
+    }
+}
---
+++ Inheritance/src/Main.java
@@ -0,0 +1,16 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        /*
+        * Inheritance = One class inherits the attributes and methods from another class.
+        * Child <- Parent <- GrandParent
+        * */
+        Dog dog = new Dog();
+        Cat cat = new Cat();
+        Plant plant = new Plant();
+
+        plant.photosynthesize();
+
+    }
+}
